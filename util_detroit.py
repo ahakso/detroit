917e182096760b27f47800b8f1e294daf9fb511e
ahakso <ahakso@bloomberg.net>
2021-08-02 10:04:28 -0700

    Add 911 calls to nb and read_csv argument option to util func

--- util_detroit.py
+++ util_detroit.py
@@ -18,9 +18,12 @@ def kml_to_gpd(fn: str):
     return gpd.read_file(f"{fn}.json/{fn}.geojson")
 
 
-def csv_with_x_y_to_gpd(fn: str, crs="EPSG:4326", drop_null_cols: bool = True):
-    """assumes projection is consistent w/ detroit files with projection. if not true, supply crs"""
-    df = pd.read_csv(fn if "csv" in fn else fn + ".csv")
+def csv_with_x_y_to_gpd(fn: str, crs="EPSG:4326", drop_null_cols: bool = True, read_csv_args: dict = {}):
+    """assumes projection is consistent w/ detroit files with projection. if not true, supply crs
+
+    optionally pass read_csv arguments convenient for reading in subset of rows w/ nrows
+    """
+    df = pd.read_csv(fn if "csv" in fn else fn + ".csv", **read_csv_args)
     if drop_null_cols:
         df = df.loc[:, df.notnull().sum() != 0]
     return gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df["X"], df["Y"])).set_crs("EPSG:4326")
