fa9d7e979d36512ad155c266d53cd3bbef4c14a8
ahakso <ahakso@bloomberg.net>
2022-02-20 21:35:24 -0800

    Add a test for the index generation for all grains and years

--- tests/test_feature_constructor.py
+++ tests/test_feature_constructor.py
@@ -21,6 +21,13 @@ class TestFeastureConstructor:
     @pytest.mark.parametrize("target_geo_grain", ["block", "block group", "tract"])
     def test_generate_index(self, decennial_census_year, target_geo_grain, partial_geo_data):
         ftr = Feature(meta={"min_geo_grain": target_geo_grain}, decennial_census_year=decennial_census_year)
-        ftr.clean_data = partial_geo_data[target_geo_grain]
         ftr.generate_index(target_geo_grain)
-        assert len(ftr.index) == BLOCKS_PER_YEAR_GEO[decennial_census_year][target_geo_grain]
+        assert (
+            len(ftr.index) == BLOCKS_PER_YEAR_GEO[decennial_census_year][target_geo_grain]
+        ), f"Index does not correspond withall geos for {target_geo_grain} in {decennial_census_year}"
+        faux_geo_data = partial_geo_data[target_geo_grain][decennial_census_year].set_index("geo_id")
+        n_geos_with_values = faux_geo_data.shape[0]
+        geos_found_in_index = faux_geo_data.reindex(ftr.index).population.notna().sum()
+        assert (
+            geos_found_in_index == n_geos_with_values
+        ), f"not all geos in sample frame contained in the index for {target_geo_grain} in {decennial_census_year}"
