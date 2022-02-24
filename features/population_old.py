0cd145e813a048a57e9c4d31315f832da6c4b75b
ahakso <ahakso@bloomberg.net>
2022-02-24 11:46:39 -0800

    deprecate population from census and replace with working values

--- features/population_old.py
+++ features/population_old.py
@@ -5,8 +5,8 @@ import pandas as pd
 from features.feature_constructor import Feature, cleanse_decorator, data_loader
 
 
-class Population(Feature):
-    """Block-level population for the census
+class PopulationOld(Feature):
+    """Block-level population for the census from the census website. For some god-forsaken reason, the 2010 census is partially missing blocks, losing 2/3 of population
 
     Arguments:
         decennial_census_year: year from which to draw the population and block_ids
@@ -19,14 +19,14 @@ class Population(Feature):
         population_data_path: Optional[str] = "",
         **kwargs,
     ) -> None:
-        if decennial_census_year == 2010:
-            source_url = "https://data2.nhgis.org/main"
-            box_url = "https://bloombergdotorg.box.com/s/qq9da9oknv6bb7pd8w802oalp27tg22b"
-            fn = "nhgis0001_ds172_2010_block.csv"
-        elif decennial_census_year == 2020:
-            source_url = "https://data2.nhgis.org/main"
-            box_url = "https://bloombergdotorg.box.com/s/6stydvct9exh1fycfxbjjvmw4kj96acb"
-            fn = "nhgis0002_ds248_2020_block.csv"
+        if decennial_census_year == 2020:
+            source_url = "https://data.census.gov/cedsci/table?q=Population%20Total&t=Counts,%20Estimates,%20and%20Projections&g=0500000US26163%241000000&tid=DECENNIALPL2020.P1"
+            box_url = "https://bloombergdotorg.box.com/s/og2qmb948k5aj7koch94kf60sfori73t"
+            fn = "DECENNIALPL2020.P1_data_with_overlays_2022-02-06T092022.csv"
+        elif decennial_census_year == 2010:
+            source_url = "https://data.census.gov/cedsci/table?q=Population%20Total&t=Counts,%20Estimates,%20and%20Projections&g=0500000US26163%241000000&tid=DECENNIALPL2010.P1"
+            box_url = "https://bloombergdotorg.box.com/s/zvsd9depnwj6nctmahhjo7baiekt86vn"
+            fn = "DECENNIALPL2010.P1_data_with_overlays_2022-02-06T092022.csv"
         else:
             raise ValueError("Year must be 2010 or 2020")
         super().__init__(
@@ -43,39 +43,20 @@ class Population(Feature):
 
     def load_data(self):
         if self.decennial_census_year == 2010:
-            cols = {
-                "H7V001": "population",
-                "STATEA": "state_code",
-                "COUNTYA": "county_code",
-                "BLOCKA": "block_code",
-                "TRACTA": "tract_code",
-            }
-            self.data = (
-                pd.read_csv(
-                    self.population_data_path + self.meta.get("filename"),
-                    usecols=cols.keys(),
-                )
-                .loc[lambda x: x.COUNTYA == 163]
-                .assign(
-                    geo_id=lambda t: t.STATEA.astype(str)
-                    + t.COUNTYA.astype(str)
-                    + t.TRACTA.astype(str)
-                    + t.BLOCKA.astype(str)
-                )
-                .loc[lambda x: x.geo_id.astype(str).str.len() == 15, ["geo_id", "H7V001"]]
-                .astype({"geo_id": float})
-                .rename(columns={"H7V001": "population"})
-            )
+            cols = {"GEO_ID": "block_id", "P001001": "population"}
         elif self.decennial_census_year == 2020:
-            cols = {"GEOCODE": "geo_id", "U7B001": "population"}
-            self.data = (
-                pd.read_csv(
-                    self.population_data_path + self.meta.get("filename"),
-                    usecols=["GEOCODE", "U7B001"],
-                )
-                .rename(columns=cols)
-                .astype({"geo_id": float})
+            cols = {"GEO_ID": "block_id", "P1_001N": "population"}
+        self.data = (
+            pd.read_csv(
+                self.population_data_path + self.meta.get("filename"),
+                usecols=cols.keys(),
+                skiprows=[1],
             )
+            .rename(columns=cols)
+            .assign(
+                geo_id=lambda x: x.block_id.str.split("US").apply(lambda s: float(s[1])),
+            )
+        )
 
         self.data = self.remove_geos_outside_detroit(self.data)
         if self.data.population.dtype == "object":
