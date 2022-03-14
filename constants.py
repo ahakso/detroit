dc1a1a7a293cb421308e754523fe2cfceaf324a6
ahakso <ahakso@bloomberg.net>
2022-03-14 15:47:01 -0700

    Add colname_map to constants.py

--- constants.py
+++ constants.py
@@ -1,1 +1,18 @@
 GEO_GRAIN_LEN_MAP = {"block": 15, "lat/long": 15, "block group": 12, "tract": 11}
+
+# Go from code-friendly to human-friendly
+# Obviously not comprehensive, but add to this as needed.
+COLNAME_MAP = {
+    "call_rate": "Gun-violence-related 911 calls/capita",
+    "married_household_prop": "Proportion of households married",
+    "rental_density": "Rental units per sq km",
+    "bus_density": "bus stops /km^2",
+    "greenlight_density": "greenlight locations/km^2",
+    "people_per_household": "People per household",
+    "violence_calls": "Gun-violence-related 911 calls",
+    "population": "Population",
+    "population_density": "Population density",
+    "per_household_income": "Per household income",
+    "out_of_state_rental_ownership": "proportion of rental units owned out of state",
+    "bus_stops": "Bus stops",
+}
