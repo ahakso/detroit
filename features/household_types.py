cfb54482b5806202fbf71bdbb596edde4ecdca54
ahakso <ahakso@bloomberg.net>
2022-03-15 12:52:33 -0700

    Correct box urls, fillna(0), and black for vinays households

--- features/household_types.py
+++ features/household_types.py
@@ -1,10 +1,9 @@
-from typing import Optional
-
-import pandas as pd
-import numpy as np
 import os
 import re
+from typing import Optional
 
+import numpy as np
+import pandas as pd
 
 from features.feature_constructor import Feature, cleanse_decorator, data_loader
 
@@ -26,7 +25,7 @@ class HouseholdTypes(Feature):
             raise ValueError("2020 data not found. Year must be 2010")
         elif decennial_census_year == 2010:
             source_url = "https://data.census.gov/cedsci/table?q=DECENNIALSF12010.P18&tid=DECENNIALSF12010.P18"
-            box_url = "https://bloombergdotorg.box.com/s/wzys9vuhjqol5mm47bv4ee92t1a0c2yi"
+            box_url = "https://bloombergdotorg.box.com/s/2r7cdthnlx8ncru4u1y9w3wp5p5yxsnq"
             fn = "DECENNIALSF12010.P18_data_with_overlays_2022-02-10T193949.csv"
         else:
             raise ValueError("Year must be 2010")
@@ -39,22 +38,21 @@ class HouseholdTypes(Feature):
                 "filename": fn,
             },
             decennial_census_year=decennial_census_year,
-            **kwargs
+            **kwargs,
         )
 
     def load_data(self):
         # this is a smaller categorical file; we can read it in
         df = pd.read_csv(os.path.join(self.data_path + self.meta.get("filename")), nrows=2)
         if self.decennial_census_year == 2010:
-            # the first non header row in the table is 
+            # the first non header row in the table is
             cols = {}
             for col in df.columns:
                 cols[col] = df[col].iloc[0]
             # cols
-            cols['GEO_ID'] = 'geo_id'
-            del cols['NAME']
-            
-            
+            cols["GEO_ID"] = "geo_id"
+            del cols["NAME"]
+
         elif self.decennial_census_year == 2020:
             cols = {}
             raise ValueError("Year must be 2010")
@@ -65,16 +63,15 @@ class HouseholdTypes(Feature):
                 skiprows=[1],
             )
             .rename(columns=cols)
-            .assign(geo_id=
-            lambda x: x.geo_id.str.split("US").apply(lambda s: s[1]))
+            .assign(geo_id=lambda x: x.geo_id.str.split("US").apply(lambda s: s[1]))
         )
-        del cols['GEO_ID']
+        del cols["GEO_ID"]
         self.features = list(cols.values())
         # cast geo_id to int
-        data['geo_id'] = data['geo_id'].astype(float)
+        data["geo_id"] = data["geo_id"].astype(float)
 
         # clean up Total column
-        data['Total'] = data['Total'].apply(lambda x: re.sub(r"\([^()]*\)", "", x)).astype(np.int64)
+        data["Total"] = data["Total"].apply(lambda x: re.sub(r"\([^()]*\)", "", x)).astype(np.int64)
         self.data = data
         self.data = self.remove_geos_outside_detroit(self.data)
 
@@ -87,12 +84,8 @@ class HouseholdTypes(Feature):
 
     @data_loader
     def construct_feature(self, target_geo_grain: str) -> pd.Series:
-        housing_types = (
-                self.assign_geo_column(target_geo_grain)
-                .groupby("geo")[self.features].sum()
-                .reindex(self.index)
-            )
-        
+        housing_types = self.assign_geo_column(target_geo_grain).groupby("geo")[self.features].sum().reindex(self.index)
+
         if self.verbose:
             print(f"{housing_types.isna().sum()} of {housing_types.shape[0]} {target_geo_grain}s are unaccounted for")
-        return housing_types
+        return housing_types.fillna(0)
--- features/household_types_ages.py
+++ features/household_types_ages.py
@@ -1,10 +1,9 @@
-from typing import Optional
-
-import pandas as pd
-import numpy as np
 import os
 import re
+from typing import Optional
 
+import numpy as np
+import pandas as pd
 
 from features.feature_constructor import Feature, cleanse_decorator, data_loader
 
@@ -26,7 +25,7 @@ class HouseholdTypesAges(Feature):
             raise ValueError("2020 data not found. Year must be 2010")
         elif decennial_census_year == 2010:
             source_url = "https://data.census.gov/cedsci/table?q=DECENNIALSF12010.P18&tid=DECENNIALSF12010.P18"
-            box_url = "https://bloombergdotorg.box.com/s/wzys9vuhjqol5mm47bv4ee92t1a0c2yi"
+            box_url = "https://bloombergdotorg.box.com/s/5q1ggv1xx466urf1ytc7q2ocpocvk51h"
             fn = "DECENNIALSF12010.P22_data_with_overlays_2022-02-10T193949.csv"
         else:
             raise ValueError("Year must be 2010")
@@ -39,22 +38,21 @@ class HouseholdTypesAges(Feature):
                 "filename": fn,
             },
             decennial_census_year=decennial_census_year,
-            **kwargs
+            **kwargs,
         )
 
     def load_data(self):
         # this is a smaller categorical file; we can read it in
         df = pd.read_csv(os.path.join(self.data_path + self.meta.get("filename")), nrows=2)
         if self.decennial_census_year == 2010:
-            # the first non header row in the table is 
+            # the first non header row in the table is
             cols = {}
             for col in df.columns:
                 cols[col] = df[col].iloc[0]
             # cols
-            cols['GEO_ID'] = 'geo_id'
-            del cols['NAME']
-            
-            
+            cols["GEO_ID"] = "geo_id"
+            del cols["NAME"]
+
         elif self.decennial_census_year == 2020:
             cols = {}
             raise ValueError("Year must be 2010")
@@ -65,16 +63,15 @@ class HouseholdTypesAges(Feature):
                 skiprows=[1],
             )
             .rename(columns=cols)
-            .assign(geo_id=
-            lambda x: x.geo_id.str.split("US").apply(lambda s: s[1]))
+            .assign(geo_id=lambda x: x.geo_id.str.split("US").apply(lambda s: s[1]))
         )
-        del cols['GEO_ID']
+        del cols["GEO_ID"]
         self.features = list(cols.values())
         # cast geo_id to int
-        data['geo_id'] = data['geo_id'].astype(float)
+        data["geo_id"] = data["geo_id"].astype(float)
 
         # clean up Total column
-        data['Total'] = data['Total'].apply(lambda x: re.sub(r"\([^()]*\)", "", x)).astype(np.int64)
+        data["Total"] = data["Total"].apply(lambda x: re.sub(r"\([^()]*\)", "", x)).astype(np.int64)
         self.data = data
         self.data = self.remove_geos_outside_detroit(self.data)
 
@@ -88,11 +85,11 @@ class HouseholdTypesAges(Feature):
     @data_loader
     def construct_feature(self, target_geo_grain: str) -> pd.Series:
         household_types_ages = (
-                self.assign_geo_column(target_geo_grain)
-                .groupby("geo")[self.features].sum()
-                .reindex(self.index)
-            )
-        
+            self.assign_geo_column(target_geo_grain).groupby("geo")[self.features].sum().reindex(self.index)
+        )
+
         if self.verbose:
-            print(f"{household_types_ages.isna().sum()} of {household_types_ages.shape[0]} {target_geo_grain}s are unaccounted for")
-        return household_types_ages
+            print(
+                f"{household_types_ages.isna().sum()} of {household_types_ages.shape[0]} {target_geo_grain}s are unaccounted for"
+            )
+        return household_types_ages.fillna(0)
