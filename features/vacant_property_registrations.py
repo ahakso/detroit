ff58ca1116570205076f336e2f8aea02d4127553
ahakso <ahakso@bloomberg.net>
2022-03-16 15:39:14 -0700

    Refactor vacant_property_registrations

--- features/vacant_property_registrations.py
+++ features/vacant_property_registrations.py
@@ -11,14 +11,13 @@ from features.feature_constructor import Feature, cleanse_decorator, data_loader
 
 class VacantPropertyRegistrations(Feature):
     # Only read in the columns we want
-    COLS_vacant_properties = [
+    COLS_VACANT_PROPERTIES = [
         "lat",
         "lon",
         "record_id",
         "date_status",
         "ObjectId",
     ]
-    TYPES_vacant_properties = [float, float, int, str, str]
 
     def __init__(
         self,
@@ -42,30 +41,28 @@ class VacantPropertyRegistrations(Feature):
     def load_data(
         self,
         sample_rows: Optional[int] = None,
-        use_lat_long: bool = False,
     ) -> None:
-        """
-        
-        """
-
-        # use a generator function to select rows we want in chunks rather than loading everything into memory at once
 
         df = pd.read_csv(
             self.data_path + self.meta.get("filename"),
             nrows=sample_rows,
-            usecols=self.COLS_vacant_properties,
-            dtype=dict(zip(self.COLS_vacant_properties, self.TYPES_vacant_properties)),
+            usecols=self.COLS_VACANT_PROPERTIES,
         )
 
-        registrations = gpd.GeoDataFrame(
-            df, geometry=gpd.points_from_xy(df.lat, df.lon), crs="epsg:4326"
-        ).rename(columns={"ObjectId": "oid"})
-        registrations = registrations.assign(
-            block_id=point_to_geo_id(
-                registrations.loc[:, ["oid", "geometry"]],
-                self.decennial_census_year,
+        registrations = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.lon, df.lat), crs="epsg:4326").rename(
+            columns={"ObjectId": "oid"}
+        )
+        registrations = (
+            registrations.assign(
+                block_id=point_to_geo_id(
+                    registrations.loc[:, ["oid", "geometry"]],
+                    self.decennial_census_year,
+                )
             )
-        ).dropna(subset=["block_id"]).astype({'block_id':float}).rename(columns={"block_id": "geo_id"})
+            .dropna(subset=["block_id"])
+            .astype({"block_id": float})
+            .rename(columns={"block_id": "geo_id"})
+        )
         self.data = registrations
         print(f"Loaded {self.data.shape[0] if sample_rows is None else sample_rows:,} rows of data")
 
@@ -86,7 +83,5 @@ class VacantPropertyRegistrations(Feature):
 
         By default, will load and cleanse data if not already done
         """
-        stations = (
-            self.assign_geo_column(target_geo_grain).groupby("geo").oid.count()
-        )
-        return stations.reindex(self.index)
+        stations = self.assign_geo_column(target_geo_grain).groupby("geo").oid.count()
+        return stations.reindex(self.index).fillna(0)
